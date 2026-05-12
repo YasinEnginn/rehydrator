e7d13d4f13976a658d749a80cf4e289bf5bfcbb1
yasin engin <yasinenginoffical@gmail.com>
2026-05-12 16:51:03 +0300

    Update firmware project files

--- old-firmware.c
+++ old-firmware.c
@@ -7,6 +7,8 @@
 
 #define LOG_MODULE "old-fw"
 #define LOG_LEVEL LOG_LEVEL_INFO
+#define U(x) ((unsigned long)(x))
+#define END_ADDR(base, size) U((base) + (size) - 1UL)
 
 PROCESS(old_firmware_process, "BIL304 HW3 old firmware");
 AUTOSTART_PROCESSES(&old_firmware_process);
@@ -18,19 +20,15 @@ PROCESS_THREAD(old_firmware_process, ev, data)
 
   PROCESS_BEGIN();
 
-  LOG_INFO("old firmware running, version=0x%08lx\n",
-           (unsigned long)HW3_OLD_FW_VERSION);
-  LOG_INFO("active app: 0x%08lx-0x%08lx\n",
-           (unsigned long)HW3_ACTIVE_APP_BASE,
-           (unsigned long)(HW3_ACTIVE_APP_BASE + HW3_ACTIVE_APP_SIZE - 1UL));
-  LOG_INFO("new image storage: 0x%08lx-0x%08lx\n",
-           (unsigned long)HW3_NEW_IMAGE_BASE,
-           (unsigned long)(HW3_NEW_IMAGE_BASE + HW3_NEW_IMAGE_SIZE - 1UL));
-  LOG_INFO("metadata: 0x%08lx-0x%08lx, ccfg: 0x%08lx-0x%08lx\n",
-           (unsigned long)HW3_UPDATE_METADATA_BASE,
-           (unsigned long)(HW3_UPDATE_METADATA_BASE + HW3_UPDATE_METADATA_SIZE - 1UL),
-           (unsigned long)HW3_CCFG_BASE,
-           (unsigned long)(HW3_CCFG_BASE + HW3_CCFG_SIZE - 1UL));
+  LOG_INFO("old firmware running, version=0x%08lx\n"
+           "active app: 0x%08lx-0x%08lx\n"
+           "new image storage: 0x%08lx-0x%08lx\n"
+           "metadata: 0x%08lx-0x%08lx, ccfg: 0x%08lx-0x%08lx\n",
+           U(HW3_OLD_FW_VERSION),
+           U(HW3_ACTIVE_APP_BASE), END_ADDR(HW3_ACTIVE_APP_BASE, HW3_ACTIVE_APP_SIZE),
+           U(HW3_NEW_IMAGE_BASE), END_ADDR(HW3_NEW_IMAGE_BASE, HW3_NEW_IMAGE_SIZE),
+           U(HW3_UPDATE_METADATA_BASE), END_ADDR(HW3_UPDATE_METADATA_BASE, HW3_UPDATE_METADATA_SIZE),
+           U(HW3_CCFG_BASE), END_ADDR(HW3_CCFG_BASE, HW3_CCFG_SIZE));
 
   etimer_set(&timer, CLOCK_SECOND * 4);
 
