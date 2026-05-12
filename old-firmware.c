13a3aa76a2fbc96d97e280d3e057aa376e17a736
yasin engin <yasinenginoffical@gmail.com>
2026-05-12 17:10:25 +0300

    kucultme

--- old-firmware.c
+++ old-firmware.c
@@ -7,6 +7,8 @@
 
 #define LOG_MODULE "old-fw"
 #define LOG_LEVEL LOG_LEVEL_INFO
+#define TO_ULONG(value) ((unsigned long)(value))
+#define RANGE_END(base, size) ((base) + (size) - 1UL)
 
 PROCESS(old_firmware_process, "BIL304 HW3 old firmware");
 AUTOSTART_PROCESSES(&old_firmware_process);
@@ -14,32 +16,34 @@ AUTOSTART_PROCESSES(&old_firmware_process);
 PROCESS_THREAD(old_firmware_process, ev, data)
 {
   static struct etimer timer;
-  static unsigned long tick;
+  static unsigned long seconds;
 
   PROCESS_BEGIN();
 
   LOG_INFO("old firmware running, version=0x%08lx\n",
-           (unsigned long)HW3_OLD_FW_VERSION);
+           TO_ULONG(HW3_OLD_FW_VERSION));
   LOG_INFO("active app: 0x%08lx-0x%08lx\n",
-           (unsigned long)HW3_ACTIVE_APP_BASE,
-           (unsigned long)(HW3_ACTIVE_APP_BASE + HW3_ACTIVE_APP_SIZE - 1UL));
+           TO_ULONG(HW3_ACTIVE_APP_BASE),
+           TO_ULONG(RANGE_END(HW3_ACTIVE_APP_BASE, HW3_ACTIVE_APP_SIZE)));
   LOG_INFO("new image storage: 0x%08lx-0x%08lx\n",
-           (unsigned long)HW3_NEW_IMAGE_BASE,
-           (unsigned long)(HW3_NEW_IMAGE_BASE + HW3_NEW_IMAGE_SIZE - 1UL));
+           TO_ULONG(HW3_NEW_IMAGE_BASE),
+           TO_ULONG(RANGE_END(HW3_NEW_IMAGE_BASE, HW3_NEW_IMAGE_SIZE)));
   LOG_INFO("metadata: 0x%08lx-0x%08lx, ccfg: 0x%08lx-0x%08lx\n",
-           (unsigned long)HW3_UPDATE_METADATA_BASE,
-           (unsigned long)(HW3_UPDATE_METADATA_BASE + HW3_UPDATE_METADATA_SIZE - 1UL),
-           (unsigned long)HW3_CCFG_BASE,
-           (unsigned long)(HW3_CCFG_BASE + HW3_CCFG_SIZE - 1UL));
+           TO_ULONG(HW3_UPDATE_METADATA_BASE),
+           TO_ULONG(RANGE_END(HW3_UPDATE_METADATA_BASE, HW3_UPDATE_METADATA_SIZE)),
+           TO_ULONG(HW3_CCFG_BASE),
+           TO_ULONG(RANGE_END(HW3_CCFG_BASE, HW3_CCFG_SIZE)));
 
-  etimer_set(&timer, CLOCK_SECOND * 4);
+  leds_on(LEDS_GREEN);
+  LOG_INFO("green LED on\n");
+
+  etimer_set(&timer, CLOCK_SECOND);
 
   while(1) {
     PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_TIMER && data == &timer);
-    tick++;
-    LOG_INFO("old firmware heartbeat %lu; waiting for preloaded new image\n",
-             tick);
-    leds_toggle(LEDS_GREEN);
+    seconds++;
+    leds_on(LEDS_GREEN);
+    LOG_INFO("old firmware: %lu. saniye; green LED on\n", seconds);
     etimer_reset(&timer);
   }
 
