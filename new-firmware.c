a75530270c2d3a952c845439296912e06123a1af
yasin engin <yasinenginoffical@gmail.com>
2026-05-14 21:14:44 +0300

    refactor: switch to ti bim oad model

--- new-firmware.c
+++ new-firmware.c
@@ -3,12 +3,12 @@
 #include "sys/log.h"
 #include "dev/leds.h"
 
-#include "hw3_image.h"
+#include "oad_layout.h"
 
 #define LOG_MODULE "new-fw"
 #define LOG_LEVEL LOG_LEVEL_INFO
 
-PROCESS(new_firmware_process, "BIL304 HW3 new firmware");
+PROCESS(new_firmware_process, "BIL304 OAD user firmware");
 AUTOSTART_PROCESSES(&new_firmware_process);
 
 PROCESS_THREAD(new_firmware_process, ev, data)
@@ -18,11 +18,11 @@ PROCESS_THREAD(new_firmware_process, ev, data)
 
   PROCESS_BEGIN();
 
-  LOG_INFO("new firmware image running, version=0x%08lx\n",
-           (unsigned long)HW3_NEW_FW_VERSION);
-  LOG_INFO("packaged image is staged at 0x%08lx and targets 0x%08lx\n",
-           (unsigned long)HW3_NEW_IMAGE_BASE,
-           (unsigned long)HW3_ACTIVE_APP_BASE);
+  LOG_INFO("OAD user firmware running, version=0x%08lx\n",
+           (unsigned long)OAD_USER_FW_VERSION);
+  LOG_INFO("BIM jumped to OAD entry 0x%08lx in staging slot 0x%08lx\n",
+           (unsigned long)OAD_STAGING_ENTRY,
+           (unsigned long)OAD_STAGING_IMAGE_BASE);
 
   etimer_set(&timer, CLOCK_SECOND * 2);
 
